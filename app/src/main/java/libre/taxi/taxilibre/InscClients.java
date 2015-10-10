3914ea22a74d46efd3ebdf5379db0776cae54bb0
mohamad yehia <mohamad.yeh@gmail.com>
2015-10-10 01:40:49 -0400

    Version 1.1 Communication avec le serveur reussi

--- app/src/main/java/libre/taxi/taxilibre/InscClients.java
+++ app/src/main/java/libre/taxi/taxilibre/InscClients.java
@@ -3,6 +3,7 @@ package libre.taxi.taxilibre;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,8 +11,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import net.sf.json.JSONObject;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class InscClients extends Activity implements TextWatcher{
+public class InscClients extends Activity implements TextWatcher {
 
     private EditText textNomClient;
     private EditText textPrenomClient;
@@ -28,6 +44,7 @@ public class InscClients extends Activity implements TextWatcher{
     String courriel;
     Button retourInsc = null;
     Context context = this;
+    static JSONObject inscriptionClient = new JSONObject();
 
     /**
      * Called when the activity is first created.
@@ -74,7 +91,14 @@ public class InscClients extends Activity implements TextWatcher{
                 passeClient = textPasseClient.getText().toString();
 
                 Clients client = new Clients(nomClient, prenomClient, telClient, utilClient, passeClient, courriel);
+                inscriptionClient.put("nomClient", client.nom);
+                inscriptionClient.put("prenomClient", client.prenom);
+                inscriptionClient.put("telClient", client.telephone);
+                inscriptionClient.put("utilClient", client.nomUtilisateur);
+                inscriptionClient.put("passeClient", client.motDePasse);
+                inscriptionClient.put("courriel", client.courriel);
 
+                new MyAsyncTask().execute();
             }
         });
 
@@ -129,5 +153,82 @@ public class InscClients extends Activity implements TextWatcher{
     @Override
     public void afterTextChanged(Editable s){
 
+    }
+
+    private class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            // TODO Auto-generated method stub
+            try {
+                postData(inscriptionClient);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public void postData(JSONObject inscriptionClient) throws IOException {
+
+        URL url = null;
+        HttpURLConnection urlConn = null;
+        DataOutputStream printout = null;
+        DataInputStream input;
+
+        url = new URL ("http://libretaxi-env.elasticbeanstalk.com/health");
+        urlConn = (HttpURLConnection) url.openConnection();
+
+        //urlConn.setDoInput (true);
+        urlConn.setDoOutput (true);
+        urlConn.setUseCaches(false);
+        urlConn.setRequestProperty("Content-Type", "application/json");
+        //urlConn.setRequestProperty("Host", "android.schoolportal.gr");
+
+        // Send POST output.
+
+        printout = new DataOutputStream(urlConn.getOutputStream ());
+        printout.writeUTF(inscriptionClient.toString());
+
+        if (urlConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            // OK
+            // otherwise, if any other status code is returned, or no status
+            // code is returned, do stuff in the else block
+            System.out.println("OK");
+        } else {
+            // Server returned HTTP error code.
+            System.out.println("NOT OK");
+        }
+
+        input = new DataInputStream(urlConn.getInputStream());
+        String  response = convertStreamToString(input);
+        System.out.println(response);
+
+        printout.flush();
+        printout.close();
+
+    }
+
+    private String convertStreamToString(DataInputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 }
